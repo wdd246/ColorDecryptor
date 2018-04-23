eb7c748f5fb7297f1a024348c43283536dd29ad2
wdd246 <adammalota999@interia.eu>
2018-04-23 22:49:00 +0200

    desc

--- code.js
+++ code.js
@@ -1,30 +1,38 @@
 function code() {
-    var ins = new Array();
-    var napis = document.getElementById("input").value;
-    var key = document.getElementById("keyin").value;
-    var block = document.getElementById("color");
-    var colors = new Array(napis.length);
+    var ins = new Array(); //array with char
+    var napis = document.getElementById("input").value; //inserted text
+    var key=100; //default key
+    
+    if (document.getElementById("keyin").value != '') { //inseted key
+        if(parseFloat(document.getElementById("keyin").value)>850 || document.getElementById("keyin").value<100)key=100;
+        else{key = document.getElementById("keyin").value;}
+    }
+    
+    var block = document.getElementById("color"); //color boxes
+    
+    //color #rgb 2D
+    var colors = new Array(napis.length); 
     for (var i = 0; i < napis.length; i++) {
         colors[i] = new Array(3);
     }
 
+    //encrypt text
     for (var i = 0; i < napis.length; i++) {
-        ins[i] = (napis.charCodeAt(i) + parseFloat(key));
-        console.log(ins[i]);
+        ins[i] = (napis.charCodeAt(i) + parseFloat(key)); //text into char + key
+        //console.log(ins[i]);
 
-        var div = document.createElement("div");
+        var div = document.createElement("div"); //createing color boxes with this settings
         div.style.width = "50px";
         div.style.height = "50px";
         div.style.background = '#' + ins[i];
         div.style.flex = "1";
         div.style.flexDirection = "row";
         div.style.display = "inline-block";
-        //div.className ="col-sm-12 col-md-4 col-lg-2 col-xl-2";
         var ck = div.style.backgroundColor;
         block.appendChild(div);
 
-        var kk = '';
-        var ki = 0;
+        var kk = ''; //string of #rgb array without 'rgb('+')'
+        var ki = 0; //color array index
 
         for (var j = 4; j < ck.length; j++) {
             if (ck[j] == ' ') {
@@ -42,7 +50,7 @@ function code() {
         ki = 0;
     }
 
-    document.getElementById('exitin').innerHTML = JSON.stringify(colors);
+    document.getElementById('exitin').innerHTML = JSON.stringify(colors); //show array with code text
     document.getElementById("input").value = '';
     //document.getElementById('exitout').innerHTML ='');
 }
--- decode.js
+++ decode.js
@@ -1,12 +1,18 @@
 function decode() {
-    var out = document.getElementById("output").value;
-    var key = document.getElementById("keyout").value;
-    var output = JSON.parse(out);
+    var out = document.getElementById("output").value; //insert text
+    var key = 100; //default key
+
+    if (document.getElementById("keyout").value != '') {//inserted key
+        if(parseFloat(document.getElementById("keyout").value)>850 || document.getElementById("keyout").value<100)key=100;
+        else{key = document.getElementById("keyout").value;} 
+    }
+    
+    var output = JSON.parse(out); //string of array into array
+    
     for (var i = 0; i < output.length; i++) {
-        var li = ((output[i][0] / 17) * 100 + (output[i][1] / 17) * 10 + output[i][2] / 17) - parseFloat(key);
-        console.log(String.fromCharCode(li));
-        document.getElementById('exitout').innerHTML += String.fromCharCode(li);
+        var li = ((output[i][0] / 17) * 100 + (output[i][1] / 17) * 10 + output[i][2] / 17) - parseFloat(key); //decoding array - key
+        document.getElementById('exitout').innerHTML += String.fromCharCode(li); //show string od output
     }
     document.getElementById("output").value = '';
-    //document.getElementById('exitin').innerHTML = '';
+
 }
